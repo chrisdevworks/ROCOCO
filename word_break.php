a6e5246b984e0b0e392d55765cc7a6e9ad7bd384
Christopher Christian Cosingan <ccosingan@gmail.com>
2024-09-06 13:20:36 +0800

    word_break_php.php

--- word_break.php
+++ word_break.php
@@ -1,43 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Word Break Problem</title>
+</head>
+<body>
+    <h1>Word Break Problem Solution</h1>
+    <?php
+    function wordBreak($s, $wordDict) {
+        global $wordSet, $memo;
+        $wordSet = array_flip($wordDict);  // Create a set for fast lookup
+        $memo = [];
+        $results = [];
 
-function wordBreak($s, $wordDict) {
-    $wordSet = array_flip($wordDict);  // Create a set for fast lookup
-    $memo = [];
-    $results = [];
+        function backtrack($start, $path) {
+            global $s, $wordSet, $results, $memo;
 
-    function backtrack($start, $path) use ($s, $wordSet, &$results, &$memo) {
-        if ($start == strlen($s)) {
-            $results[] = implode(' ', $path);
-            return;
-        }
-
-        if (isset($memo[$start])) {
-            return;
-        }
-
-        for ($end = $start + 1; $end <= strlen($s); $end++) {
-            $word = substr($s, $start, $end - $start);
-            if (isset($wordSet[$word])) {
-                $path[] = $word;
-                backtrack($end, $path);
-                array_pop($path);
+            if ($start == strlen($s)) {
+                $results[] = implode(' ', $path);
+                return;
             }
+
+            if (isset($memo[$start])) {
+                return;
+            }
+
+            for ($end = $start + 1; $end <= strlen($s); $end++) {
+                $word = substr($s, $start, $end - $start);
+                if (isset($wordSet[$word])) {
+                    $path[] = $word;
+                    backtrack($end, $path);
+                    array_pop($path);
+                }
+            }
+
+            $memo[$start] = true;
         }
 
-        $memo[$start] = true;
+        backtrack(0, []);
+        return $results;
     }
 
-    backtrack(0, []);
-    return $results;
-}
+    // Input
+    $wordDict = ["this", "th", "is", "famous", "Word", "break", "b", "r", "e", "a", "k", "br", "bre", "brea", "ak", "problem"];
+    $s = "Wordbreakproblem";
 
-// Input
-$wordDict = ["this", "th", "is", "famous", "Word", "break", "b", "r", "e", "a", "k", "br", "bre", "brea", "ak", "problem"];
-$s = "Wordbreakproblem";
+    // Function call and output
+    $result = wordBreak($s, $wordDict);
+    ?>
 
-// Function call and output
-$result = wordBreak($s, $wordDict);
-foreach ($result as $sentence) {
-    echo $sentence . "\n";
-}
-?>
+    <ul>
+        <?php foreach ($result as $sentence): ?>
+            <li><?php echo htmlspecialchars($sentence); ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
